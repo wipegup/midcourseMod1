38b49939d4c3a84f025751ac80101d9c16040d2a
wipegup <wipegup@gmail.com>
2019-02-10 15:05:10 -0700

    Iteration 1 tests passing

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,6 +1,9 @@
 class Employee
+  attr_reader :name, :age, :salary
   def initialize(info_hash)
-    
+    @name = info_hash[:name]
+    @age = info_hash[:age].to_i
+    @salary = info_hash[:salary].to_i
 
   end
 end
