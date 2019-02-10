8196f528a99033cdf5294c9dd5948b574a3019f7
wipegup <wipegup@gmail.com>
2019-02-10 15:17:39 -0700

    add test it exists ;department

--- test/department_test.rb
+++ test/department_test.rb
@@ -4,9 +4,10 @@ require './lib/employee'
 
 class DepartmentTest < MiniTest::Test
   def setup
-
     @department = Department.new("Customer Service")
   end
 
-  
+  def test_it_exists
+    assert_instance_of Department, @department
+  end
 end
