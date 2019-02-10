d04c2c9451c2629277259889af354e8fc08ce366
wipegup <wipegup@gmail.com>
2019-02-10 14:56:22 -0700

    Add test for it exists; Employee

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -2,11 +2,13 @@ require 'minitest/autorun'
 require './lib/employee'
 class EmployeeTest < MiniTest::Test
   def setup
-
     @employee = Employee.new({name:"Bobbi Jaeger",
                               age: "30",
                               salary: "100000"})
   end
 
-  
+  def test_it_exists
+    assert_instance_of Employee, @employee
+  end
+  end
 end
